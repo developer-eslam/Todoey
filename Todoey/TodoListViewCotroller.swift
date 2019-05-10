43ea76b9f02ce4c4aabe45269f4cbc08d49b7cc3
Eslam Abotaleb <eslamabotaleb12@hotmail.com>
2019-05-10 08:02:54 -0700

    Data Persisted Defaults method

--- Todoey/TodoListViewCotroller.swift
+++ Todoey/TodoListViewCotroller.swift
@@ -10,6 +10,27 @@ import UIKit
 
 class TodoListViewCotroller: UITableViewController {
 
+    var itemarray = ["First", "Second", "Third"]
+    
+    let defaults = UserDefaults.standard
+    @IBAction func addbtnPressed(_ sender: Any) {
+        
+        
+        var textfield = UITextField()
+        let alert = UIAlertController(title: "Add New Todo Item", message: " ", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            self.itemarray.append(textfield.text!)
+            self.defaults.set(self.itemarray, forKey: "ItemsData")
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alerttextfield) in
+            alerttextfield.placeholder = "Create New Item"
+            textfield = alerttextfield
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,10 +39,33 @@ class TodoListViewCotroller: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        if let  item = defaults.array(forKey: "ItemsData") as? [String] {
+            itemarray = item
+        }
+        
     }
 
     // MARK: - Table view data source
 
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+
+        }
+       
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "todoelist")
+        cell!.textLabel?.text = itemarray[indexPath.row]
+        return cell!
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemarray.count
+    }
 
     
 }
