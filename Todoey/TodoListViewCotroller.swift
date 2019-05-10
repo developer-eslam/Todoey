242ea5a838033feaf35e3b61e83068b78c42ea5d
Eslam Abotaleb <eslamabotaleb12@hotmail.com>
2019-05-10 06:33:43 -0700

    TodoeyList With Tableview

---
+++ Todoey/TodoListViewCotroller.swift
@@ -0,0 +1,27 @@
+//
+//  TodoListViewCotroller.swift
+//  Todoey
+//
+//  Created by Eslam Abotaleb on 5/10/19.
+//  Copyright © 2019 Eslam Abotaleb. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewCotroller: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+
+    
+}
